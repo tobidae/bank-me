a5e6518b72632750880f042ca655d6a8cb264e59
Tobi Akerele <otakerele@gmail.com>
2020-10-04 19:23:00 -0500

    Add firebase trigger and update entry file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,41 @@
-import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as constants from "./utilities/const";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import triggers from './triggers';
+import * as noauth from './utilities/noauth-functions';
+import {VALID_MEMORY_OPTIONS} from 'firebase-functions/lib/function-configuration';
+
+const main = express();
+require('./utilities').default({expressApp: main});
+
+const triggerPaths = triggers.paths;
+// const pubsubTriggers = triggers.pubsub;
+// const rtDbTriggers = triggers.database;
+// const firestoreTriggers = triggers.firestore;
+
+const runtimeOpts256 = {
+  memory: VALID_MEMORY_OPTIONS[1] // 256 MB
+};
+const runtimeOpts512 = {
+  memory: VALID_MEMORY_OPTIONS[2] // 512 MB
+};
+const runtimeOpts1024 = {
+  memory: VALID_MEMORY_OPTIONS[3] // 1024 MB
+};
+const timezone = "America/Chicago";
+
+const functions = constants.default.functions;
+const funcOpts = functions.runWith(runtimeOpts256);
+const funcFirestore = funcOpts.firestore.document;
+const funcDatabase = funcOpts.database.ref;
+
+// HTTP REQUESTS
+export const api = functions.runWith(runtimeOpts512).https.onRequest(main);
+
+// FIRESTORE
+// When a store is created and the user enabled the inventoryFeed Service, create the defaultFeed
+
+
+// RT DATABASE
+
+// PUB SUB
---
+++ functions/src/triggers/index.ts
@@ -0,0 +1,16 @@
+// import * as database from './database';
+// import * as firestore from './firestore';
+// import * as pubsub from './pubsub';
+
+const paths = {
+    user: '/users/{userID}',
+    settings: '/settings/{userID}',
+};
+
+
+export default {
+    // database,
+    // firestore,
+    // pubsub,
+    paths
+}
